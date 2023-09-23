9e42398d66419b9408fc7c4c471299ae3c1c9051
sifat <SifatSikder>
2023-09-24 03:13:37 +0600

    splitted in microservices

---
+++ Linkdin-1/Backend/Backend-with-docker-1/UserService/index.js
@@ -0,0 +1,31 @@
+//App creation
+const express = require("express");
+const app = express();
+
+//port setup
+const port = process.env.PORT || 8000;
+
+//body parser configutation
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//cors setup
+const cors = require("cors");
+app.use(cors())
+
+
+const mongoose = require("mongoose");
+
+//database connection
+const connectDB = require('./config/db')
+connectDB();
+
+
+const userRoutes = require('./routes/user');
+app.use('/api/user', userRoutes);
+
+
+
+//server listens here
+app.listen(port, () => { console.log(`server is runing at port ${port}`) }) 
---
+++ Linkdin-1/Backend/Backend-with-docker-1/UserService/routes/user.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+
+require('dotenv').config()
+
+router.post("/register", async (req, res) => {
+
+
+    //Email and password is necessary to register    
+    if ((!req.body.email) || (!req.body.password)) {
+        return res.json({ success: false, message: 'Enter all fields' })
+    }
+    else {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) return res.json({ success: false, message: "User already exists" });
+        else {
+            let newUser = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+            });
+            await newUser.save()
+            return res.json({ success: true, message: 'User Registration Successful' })
+        }
+    }
+});
+
+router.post("/login", async (req, res) => {
+
+
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(403).json({ success: false, message: 'Authentication Failed, User not found' })
+    }
+    else {
+        user.comparePassword(req.body.password, function (err, isMatch) {
+            if (isMatch && !err) {
+                // var token = jwt.encode(user, process.env.PASSWORD_HASH_SECRET_KEY)
+                const userInfo = { id: user._id, email: user.email, password: user.password };
+                console.log(userInfo);
+                var token = jwt.sign(userInfo, process.env.PASSWORD_HASH_SECRET_KEY)
+                return res.json({ success: true, message: token })
+            }
+            else {
+                return res.status(403).json({ success: false, message: 'Authentication failed, wrong password' })
+            }
+        })
+    }
+});
+
+function authenticateToken(req, res, next) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+
+        var token = req.headers.authorization.split(' ')[1];
+
+        if (token == 'null') {
+            return res.json({ success: false, message: 'No Token Provided' });
+        }
+        jwt.verify(token, process.env.PASSWORD_HASH_SECRET_KEY, (err, userInfo) => {
+            if (err) {
+                return res.status(403).json({ success: false, message: 'Invalid Token' });
+            }
+            else {
+                req.user = userInfo;
+                next();
+            }
+        })
+    }
+    else {
+        return res.json({ success: false, message: 'Access denied' });
+    }
+
+}
+
+
+module.exports = router;
